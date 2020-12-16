5c21294a15f2014182038ca136c3e0a37a50d86d
aditee-singh <singhaditee33@gmail.com>
2020-12-16 22:30:27 +0530

    Added Loader and disabled buttons while request is pending

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 
             function searchFunction(){
+                document.getElementById("searchButton").disabled = true;
+                document.getElementById("randomButton").disabled = true;
+                document.getElementById("gif").src="loading.webp";
+                document.getElementById('gif').style.display='block';
                 var giphy_name=document.getElementById("searchitem").value;
                 var url="https://api.giphy.com/v1/gifs/search?api_key=EIkbwVRNCK8YVEfOVdMHG3ujfSCFjGVO&q="+giphy_name+"&limit=1";
                 console.log(url);
                 fetch(url)
                 .then(response=>response.json())
-                .then(content=>{
+                .then(content=>{     
                     console.log(content.data)
-                    document.getElementById("gif").src=content.data[0].images.fixed_height_still.url;
-                    document.getElementById("gif").props=content.data[0].images.downsized.url;
+                    document.getElementById("gif").src=content.data[0].images.downsized.url;
+                    document.getElementById("gif").props=content.data[0].images.fixed_height_still.url;
+                    document.getElementById("searchButton").disabled = false;
+                    document.getElementById("randomButton").disabled = false;
                     document.getElementById('gif').style.display='block';
+                    document.getElementById('searchitem').value="";
                 })
                 .catch((err)=>console.log(err));
-            }
+                };
             function playPause(){
                 var temp=document.getElementById("gif").src;
                 document.getElementById("gif").src=document.getElementById("gif").props;
                 document.getElementById("gif").props=temp;
             }
             function randomFunction() {
-                var url="https://api.giphy.com/v1/gifs/random?&api_key=dc6zaTOxFJmzC&limit=1";    
+                document.getElementById("gif").src="loading.webp";
+                document.getElementById('gif').style.display='block';
+                var url="https://api.giphy.com/v1/gifs/random?&api_key=EIkbwVRNCK8YVEfOVdMHG3ujfSCFjGVO&limit=1";    
                 fetch(url)
                 .then(response=>response.json())
                 .then(content=>{
                     console.log(content.data);
-                    document.getElementById("gif").src = content.data.images.fixed_height_still.url;
-	                document.getElementById("gif").props = content.data.images.downsized.url;
-  })
+                    document.getElementById("gif").src = content.data.images.downsized.url;
+                    document.getElementById("gif").props =content.data.images.fixed_height_still.url;
+                    document.getElementById('gif').style.display='block'; 
+                })
   .catch((err) => alert(err));
-   
-}
-                
+} 
